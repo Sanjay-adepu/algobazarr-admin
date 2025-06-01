043fd91e445deb0c37cea6aaf9199e58d8e91fb9
Sanjay-adepu <adepusanjay812@gmail.com>
2025-06-01 20:58:44 +0530

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-
 const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
